257aa20f0e25186a79e3c143f81389e36a83aa42
Shyam Kumar <20shyamkumar07@gmail.com>
2026-03-06 22:53:35 -0500

    updated setup to comply wiht airport data

--- backend/app/tempest/__init__.py
+++ backend/app/tempest/__init__.py
@@ -1,7 +1,8 @@
 """Tempest backend core modules."""
 
+from .airport import get_airport
 from .metar import get_latest_metar
 from .minimums_store import JsonMinimumsStore
 from .taf import get_latest_taf
 
-__all__ = ["get_latest_metar", "get_latest_taf", "JsonMinimumsStore"]
+__all__ = ["get_latest_metar", "get_latest_taf", "get_airport", "JsonMinimumsStore"]
